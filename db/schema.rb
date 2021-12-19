6076a0270c29d29bd31c5854eca6a92b222a1eb1
Jonathan Pahl <jdpahl122@gmail.com>
2021-12-19 16:49:21 -0500

    Add Teams

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_19_194716) do
+ActiveRecord::Schema.define(version: 2021_12_19_214910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,5 +29,14 @@ ActiveRecord::Schema.define(version: 2021_12_19_194716) do
     t.index ["conference_id"], name: "index_divisions_on_conference_id"
   end
 
+  create_table "teams", force: :cascade do |t|
+    t.string "team_name"
+    t.bigint "division_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["division_id"], name: "index_teams_on_division_id"
+  end
+
   add_foreign_key "divisions", "conferences"
+  add_foreign_key "teams", "divisions"
 end
